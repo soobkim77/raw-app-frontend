0acf4bdac322de9336279f92af719c53845bad6f
Soo-Beom <soobeomkim77@gmail.com>
2021-05-11 16:17:44 -0700

    added basic comment post on a specific blog

--- src/App.js
+++ src/App.js
@@ -100,6 +100,7 @@ class App extends React.Component {
     fetch(BLOGURL, configObj)
     .then(r => r.json())
     .then(resp => {
+      console.log(resp)
       this.setState({blogs: resp.data})
     })
     
