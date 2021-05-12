905ae745c298adf4b61ebcbd0a2ff1cf91670857
Roman Turner <rn.turner@live.com>
2021-05-11 22:28:50 -0700

    testing

--- src/components/BlogSnip.js
+++ src/components/BlogSnip.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import {Link} from 'react-router-dom'
-import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom'
+//Material UI
+import {
+  Typography,
+  IconButton,
+  CardContent,
+  CardActions,
+  Card,
+  makeStyles,
+} from "@material-ui/core/";
+
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import YoutubeSearchedForOutlinedIcon from "@material-ui/icons/YoutubeSearchedForOutlined";
 
 const useStyles = makeStyles({
   root: {
@@ -30,21 +39,44 @@ export default function BlogSnip(props) {
   return (
     <Card className={classes.root}>
       <CardContent>
-
-        <Typography variant="h5" component="h2">
+        <Typography variant='h5' component='h2'>
           {props.blog.attributes.title}
         </Typography>
-        <Typography className={classes.pos} color="textSecondary">
+        <Typography className={classes.pos} color='textSecondary'>
           {props.blog.attributes.user}
         </Typography>
-        <Typography variant="body2" component="p">
+        <Typography variant='body2' component='p'>
           well meaning and kindly.
           <br />
           {/* {blog.img === "" ? null : <img src={blog.img} alt="" />} */}
         </Typography>
       </CardContent>
+
       <CardActions>
-        <Link to={`/blogs/${props.blog.id}`} onClick={() => props.showBlog(props.blog)}>Read More</Link>
+        {/* <Link
+          to={`/blogs/${props.blog.id}`}
+          onClick={() => props.showBlog(props.blog)}
+        >
+          Read More
+        </Link> */}
+        <IconButton
+          component={Link}
+          to={`/blogs/${props.blog.id}`}
+          aria-label='show'
+        >
+          <YoutubeSearchedForOutlinedIcon
+            onClick={() => props.showBlog(props.blog)}
+          />
+        </IconButton>
+        <IconButton aria-label='edit'>
+          <EditIcon />
+        </IconButton>
+        <IconButton aria-label='delete'>
+          <DeleteForeverIcon />
+        </IconButton>
+        <IconButton aria-label='love'>
+          <FavoriteBorderOutlinedIcon />
+        </IconButton>
       </CardActions>
     </Card>
   );
