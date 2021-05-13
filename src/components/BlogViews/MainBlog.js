c1f072135e6166a6fe1473ce17ba8b70a4d81224
Roman Turner <rn.turner@live.com>
2021-05-13 11:28:59 -0700

    display blog

--- src/components/BlogViews/MainBlog.js
+++ src/components/BlogViews/MainBlog.js
@@ -1,117 +1,84 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import DisplayBlog from "./DisplayBlog";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import Link from "@material-ui/core/Link";
+
+const URL = "http://localhost:3000/blogs/";
+
+const initialState = {
+  content:
+    "Amorphous comprehension tenebrous immemorial cat nameless. Gibbering accursed singular hideous iridescence. Lurk tentacles unmentionable decadent antiquarian ululate accursed.Anmentionable decadent antiquarian ululate accursed.Amorphous comprehension tenebrous immemorial cat nameless. Gibbering accursed singular hideous iridescence. Lurk tentacles unmentionable decadent antiquarian ululate accursed.",
+  created_at: "2021-05-12T20:30:07.124Z",
+  id: "176",
+  img: "https://picsum.photos/200/300",
+  title: "Praise Kai!",
+  user: "Miles Teg",
+};
+
 
 const useStyles = makeStyles((theme) => ({
-  mainFeaturedBlog: {
-    position: "relative",
-    backgroundColor: theme.palette.grey[800],
-    color: theme.palette.common.white,
-    marginBottom: theme.spacing(4),
-    backgroundImage: "url(https://source.unsplash.com/random)",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
+  root: {
+    flexGrow: 1,
   },
-  overlay: {
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    backgroundColor: "rgba(0,0,0,.3)",
+  paper: {
+    height: 140,
+    width: 100,
   },
-  mainFeaturedBostContent: {
-    position: "relative",
-    padding: theme.spacing(3),
-    [theme.breakpoints.up("md")]: {
-      padding: theme.spacing(6),
-      paddingRight: 0,
-    },
+  control: {
+    padding: theme.spacing(2),
   },
 }));
 
-const URL = "http://localhost:3000/blogs/";
-const initialState = {
-  title: "default",
-  img: "default",
-  content: "default",
-};
-
 export default function MainBlog() {
-    const classes = useStyles();
-    const [blog, setBlog] = useState(initialState);
-    const [comments, setComments] = useState([]);
-    const { id } = useParams();
+  const classes = useStyles();
+  const [blog, setBlog] = useState(initialState);
+  const [comments, setComments] = useState([]);
+  const { id } = useParams();
 
-    useEffect(() => {
-      let configObj = {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-        },
-      };
-
-      fetch(URL + id, configObj)
-        .then((r) => r.json())
-        .then((data) => sanitize(data))
-        .catch((e) => console.error("e:", e));
-    }, []);
-
-    const sanitize = (data) => {
-      const newBlog = {
-        id: data.data.id,
-        content: data.data.attributes.content,
-        created_at: data.data.attributes.created_at,
-        img: data.data.attributes.img,
-        title: data.data.attributes.title,
-        user: data.data.attributes.user,
-      };
-      const newComments = {
-        comments: data.data.attributes.comments.data,
-      };
-      setBlog(newBlog);
-      setComments(newComments);
+  useEffect(() => {
+    let configObj = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
     };
 
+    fetch(URL + id, configObj)
+      .then((r) => r.json())
+      .then((data) => sanitize(data))
+      .catch((e) => console.error("e:", e));
+  }, []);
+
+  const sanitize = (data) => {
+    const newBlog = {
+      id: data.data.id,
+      content: data.data.attributes.content,
+      created_at: data.data.attributes.created_at,
+      img: data.data.attributes.img,
+      title: data.data.attributes.title,
+      user: data.data.attributes.user,
+    };
+    const newComments = {
+      comments: data.data.attributes.comments.data,
+    };
+    setBlog(newBlog);
+    setComments(newComments);
+  };
+
   return (
-    <Paper
-      className={classes.mainFeaturedBlog}
-      style={{ backgroundImage: `url(${blog.image})` }}
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justify='center'
+      style={{ minHeight: "80vh" }}
     >
-      {
-        <img
-          style={{ display: "none" }}
-          src={blog.image}
-          alt={blog.imageText}
-        />
-      }
-      <div className={classes.overlay} />
-      <Grid container>
-        <Grid item md={6}>
-          <div className={classes.mainFeaturedBlogContent}>
-            <Typography
-              component='h1'
-              variant='h3'
-              color='inherit'
-              gutterBottom
-            >
-              {blog.title}
-            </Typography>
-            <Typography variant='h5' color='inherit' paragraph>
-              {blog.content}
-            </Typography>
-            <Link variant='subtitle1' href='#'>
-              {blog.title}
-            </Link>
-          </div>
-        </Grid>
+      <Grid item xs={8}>
+        <DisplayBlog blog={blog} />
       </Grid>
-    </Paper>
+    </Grid>
   );
 }
